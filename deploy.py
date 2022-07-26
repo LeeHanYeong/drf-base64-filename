df79ad4f2b5078e2e2fcd31f9a3ec09b5f189f52
lhy <dev@lhy.kr>
2022-07-26 10:51:59 +0900

    Fix deploy settings

--- deploy.py
+++ deploy.py
@@ -2,22 +2,14 @@
 import subprocess
 import os
 
-ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
-VERSION_FILE = os.path.join(ROOT_DIR, 'version.txt')
+ROOT_DIR = os.path.dirname(__file__)
 
 if __name__ == '__main__':
-    # Write new version
-    previous_version = open(VERSION_FILE).read()
-    main, minor = previous_version.rsplit('.', 1)
-    new_version = '{main}.{minor}'.format(
-        main=main,
-        minor=int(minor) + 1
-    )
-    open(VERSION_FILE, 'wt').write(new_version)
-
     # Export requirements.txt
-    subprocess.run('poetry export -f requirements.txt requirements.txt', shell=True)
+    subprocess.run('cd test_project && poetry export --without-hashes -o requirements.txt', shell=True)
+    # Remove old archives
+    subprocess.run('rm -rf dist', shell=True)
     # Generating distribution archives
-    subprocess.run('python setup.py sdist bdist_wheel', shell=True)
+    subprocess.run('python -m build', shell=True)
     # Upload the distribution archives
     subprocess.run('twine upload dist/*', shell=True)
